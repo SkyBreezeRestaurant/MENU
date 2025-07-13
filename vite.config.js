90f45cab7a0a836e856e7a59c7a2da1a92899743
Lalit Patel <lalitpatel0706@gmail.com>
2025-07-14 00:00:05 +0530

    Update config for new GitHub account deployment

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'MENU', // replace 'my-app' with your actual repo name
+  base: '/MENU/', // replace 'my-app' with your actual repo name
 })
